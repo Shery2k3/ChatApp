6ccb24678ca1fd861bb2de8a6a591a422ddc3e32
Shahryar Khan <shahryar2k3@gmail.com>
2023-05-04 02:07:57 +0500

    Made connections with consumers

--- consumers.py
+++ consumers.py
@@ -2,4 +2,32 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
-    pass
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']   # get room name from url
+        self.room_group_name = 'chat_' + self.room_name     # create group name
+
+        await self.channel_layer.group_add(    # add channel to group
+            self.room_group_name,
+            self.channel_name
+        )   
+        
+        await self.channel_layer.group_send(    # send message to group
+            self.room_group_name,
+            {
+                'type': 'tester_message',
+                'tester': 'testing',
+            }
+        )
+    
+    async def tester_message(self, event):    # receive test message from group
+        tester = event['tester']
+
+        await self.send(text_data=json.dumps({
+            'tester': tester,
+        }))
+
+    async def disconnect(self):    # remove channel from group
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
