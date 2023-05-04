50184d2ffe1da91218021320da050e3ca4c03f84
Shahryar Khan <shahryar2k3@gmail.com>
2023-05-05 02:36:57 +0500

    Update views.py

--- views.py
+++ views.py
@@ -5,6 +5,6 @@ def index(request):
     return render(request, 'index.html')
 
 def room(request, room_name):
-    return render(request, 'room.html', {
+    return render(request, 'chatroom.html', {
         'room_name': room_name
     })
