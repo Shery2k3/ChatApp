ced3e452192613bc014df048df1b9086fc345306
Muhammad Sheryar Khan <shahryar2k3@gmail.com>
2023-08-19 21:49:55 +0500

    Setup templates

--- core/views.py
+++ core/views.py
@@ -1,4 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import SignupForm
 
-def frontpage(request):
-    return render(request, 'core/frontpage.html')
+def home(request):
+    return render(request, 'home.html')
+
+def login_page(request):
+    return render(request, 'login.html')
+
+def signup_page(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            # save the user object
+            user = form.save()
+            # log the user in
+            login(request, user)
+            # redirect to home
+            return redirect('home')
+    else:
+        form = SignupForm()
+
+    return render(request, 'signup.html', {'form': form})
