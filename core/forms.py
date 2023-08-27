fc352d8fb285142a71388764d87901ab3d488639
Muhammad Sheryar Khan <shahryar2k3@gmail.com>
2023-08-27 23:31:16 +0500

    Signup page integrated

--- core/forms.py
+++ core/forms.py
@@ -1,7 +1,16 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-class SignupForm(UserCreationForm):
+class RegisterForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = ''
+        self.fields['password1'].label = ''
+        self.fields['password2'].label = ''
+        print(self.fields['password1'].help_text)
+        self.fields['password1'].help_text = ''
+        self.fields['password2'].help_text = ''
+        self.fields['username'].help_text = ''
     class Meta:
-        model=User
-        fields=['username','email','password1','password2']
+        model = User
+        fields = ['username', 'password1', 'password2']
--- core/views.py
+++ core/views.py
@@ -1,24 +1,21 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
-from .forms import SignupForm
+from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterForm
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'core/home.html')
 
 def login_page(request):
-    return render(request, 'login.html')
+    return render(request, 'core/login.html')
 
-def signup_page(request):
-    if request.method == 'POST':
-        form = SignupForm(request.POST)
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
         if form.is_valid():
-            # save the user object
-            user = form.save()
-            # log the user in
-            login(request, user)
-            # redirect to home
+            form.save()
             return redirect('home')
     else:
-        form = SignupForm()
-
-    return render(request, 'signup.html', {'form': form})
+        form = RegisterForm()
+    
+    return render(request, 'core/register.html', {'form': form})
